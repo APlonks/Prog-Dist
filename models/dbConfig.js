4830142d966b70b036c832aa7b9b7a96d53a842b
APlonks <antoine.planque@gmail.com>
2022-01-31 09:47:11 +0100

    Update lien vers la BDD

--- models/dbConfig.js
+++ models/dbConfig.js
@@ -1,9 +1,12 @@
 //Fichier de configuraiton pour se connecter à la base de données
 
+lienMongo = "mongodb://localhost:27017/MoiTestDB"   // Quand on est en localhost
+//lienMongo = "mongodb://mongo:27017/MoiTestDB"   // Quand on déploie sur docker
+
 const mongoose = require ('mongoose')   //pour récuperer toutes les méthodes du package mongoose dans la constante mongoose
 
 mongoose.connect(   //Connexion à la BDD
-    "mongodb://localhost:27017/MoiTestDB",  //Le lien vers la base de données Mongodb
+    lienMongo,  //Le lien vers la base de données Mongodb
     { useNewUrlParser: true, useUnifiedTopology: true},
     (err) => {
         if (!err) console.log("Mongodb connected"); //  Si il n'y pas d'erreur pour la connexion à mongodb
